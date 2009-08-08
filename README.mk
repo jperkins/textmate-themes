081d5b3d4144720db46a51a558b7cceb92d449e8
Jason Perkins <jperkins@sneer.org>
2009-08-07 19:27:49 -0500

    updated readme

---
+++ README.mk
@@ -0,0 +1,15 @@
+
+A couple of custom TextMate themes.
+
+
+## Gruber Dark
+
+Derived from the [BBEdit Theme](http://daringfireball.net/projects/bbcolors/schemes/)
+of the same name. Original text formatting was 10 point Monaco with no antialiasing.
+
+
+
+## Dekorte
+
+Based on the XCode theme of [Steven Dekorte](http://dekorte.com/blog/blog.cgi).
+Original text formatting was 13 point Courier with no antialiasing.
